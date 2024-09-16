b222b2e8d9e422252750796a20b91a7755826d7b
QuanDH <rickymta@gmail.com>
2024-09-16 23:01:05 +0700

    first commit

---
+++ src/Volcanion.Utilities.Chatbot/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace Volcanion.Utilities.Chatbot;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+            .Build();
+
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+        Log.Information("Ứng dụng bắt đầu chạy");
+
+        // Chạy ứng dụng
+        try
+        {
+            Log.Information("Chạy chương trình");
+            // Logic của ứng dụng
+        }
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Ứng dụng kết thúc đột ngột");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
+    }
+}
---
+++ test/Volcanion.Utilities.Chatbot.Test/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Volcanion.Utilities.Chatbot.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
